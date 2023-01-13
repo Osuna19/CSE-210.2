f3fce944816f67e36dc5c400244066c19145fa4a
Ian Osuna <osu19002@byui.edu>
2023-01-13 15:29:56 -0700

    Prep 3 guess  Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random generator = new Random();
+        int magicNumber = generator.Next(1, 11);
+
+        bool keepPlaying = true;
+
+        while(keepPlaying)
+        {
+            Console.Write("Guess a number between 1 and 10: ");
+            int userGuess = int.Parse(Console.ReadLine() ??"");
+
+            if (magicNumber == userGuess)
+            {
+                Console.WriteLine("Correct, nice!");
+                keepPlaying = false;
+            }
+            else if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+
+
+
+        //Console.Write("what is your magical number? ");
+        //string magiNumber = Console.ReadLine();
+        //Console.Write("what is your number? ");
+        //string guessNumber = Console.ReadLine();
+
+        //int x = int.Parse(magicNumber);
+        //int y = int.Parse(guessNumber);
+
+        //if (x > y)
+        //{
+       //     Console.WriteLine("Higher");
+       // }
+       // else if (x < y)
+       // {
+//Console.WriteLine("Lower");
+       // }
+       // else
+       // {
+       //     Console.WriteLine("Correct");
+       // }
+
     }
+
+
 }
