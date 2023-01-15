f82cf4edb17f63739b6c19ddbb573d468748bc61
Ian Osuna <osu19002@byui.edu>
2023-01-14 18:14:40 -0700

    All preps done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
+  
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+
+        string userName = TheUserName();
+        int userNumber = TheUserNumber();
+        int squareNumber = SquareUserNumber(userNumber);
+
+        GiveResult(userName, squareNumber);
     }
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string TheUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int TheUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareUserNumber(int number)
+    {
+        int answer = number * number;
+        return answer;
+    }
+
+    static void GiveResult(string name, int answer)
+    {
+        Console.WriteLine($"{name}, the square of your number is {answer}");
+    }
+
+
 }
