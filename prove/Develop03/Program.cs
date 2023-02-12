ce559fb1defebffd8c9a569b125ebdc8b4c2d504
Ian Osuna <osu19002@byui.edu>
2023-02-11 20:32:35 -0700

    a ver si jala

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture verse = new Scripture("For behold, this is my work and my glory—to bring to pass the immortality and eternal life of man.");
+        Word word = new Word(verse);
+        Reference chapter = new Reference();
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(chapter.getReference());
+            Console.WriteLine(verse.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to replace another word. Type quit to end program.");
+            string endProgram = Console.ReadLine();
+            if (endProgram == "quit"){
+                break;
+            }
+
+            Console.ReadLine();
+            word.replaceWord();
+
+            if (verse.IsCompletelyReplaced)
+            {
+                
+                Console.WriteLine("All words are replaced.");
+                Console.WriteLine("Did you memorize the scripture? If yes type the scripture to see if you memorize it:");
+                string memory = Console.ReadLine();
+                if (memory == verse.OriginalVerse){
+                    Console.WriteLine("You learned a new scripture!");
+                }
+                else {
+                    Console.WriteLine("Oh, you almost got it! the scripture was" + verse.OriginalVerse);
+                }
+                break;
+            }
+        }
     }
 }
